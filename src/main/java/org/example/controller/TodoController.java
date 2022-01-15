6ffa003529f6d61caa712f3de97518e836367f7d
jhyunlee94 <jkwhd12@gmail.com>
2022-01-15 12:51:16 +0900

    0115 api 설계

--- src/main/java/org/example/controller/TodoController.java
+++ src/main/java/org/example/controller/TodoController.java
@@ -2,13 +2,16 @@ package org.example.controller;
 
 
 import lombok.AllArgsConstructor;
+import org.example.model.TodoEntitiy;
+import org.example.model.TodoRequest;
 import org.example.model.TodoResponse;
 import org.example.service.TodoService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.SQLOutput;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @CrossOrigin // cors라는 이슈를 막기 위해서 사용
@@ -20,39 +23,59 @@ public class TodoController {
     private final TodoService service;
 
     @PostMapping
-    public ResponseEntity<TodoResponse> create(){
+    public ResponseEntity<TodoResponse> create(@RequestBody TodoRequest request){
         System.out.println("CREAT");
-        return null;
+        //request 에서 타이틀이없으면 정상적인 값을 알수없다
+        if(ObjectUtils.isEmpty(request.getTitle()))
+            return ResponseEntity.badRequest().build();
+
+        if(ObjectUtils.isEmpty(request.getOrder()))
+            request.setOrder(0L); // default 값
+
+        if(ObjectUtils.isEmpty(request.getCompleted()))
+            request.setCompleted(false);
+
+        TodoEntitiy result = this.service.add(request);
+
+        return ResponseEntity.ok(new TodoResponse(result)); //받은 리스폰스를 투두리스폰스에 한번 매핑해서 내려줍니다.
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<TodoResponse> readOne(){
+    public ResponseEntity<TodoResponse> readOne(@PathVariable Long id){
         System.out.println("READ ONE");
-        return null;
+        TodoEntitiy result = this.service.searchById(id);
+
+        return ResponseEntity.ok(new TodoResponse(result));
     }
 
     @GetMapping
     public ResponseEntity<List<TodoResponse>> readAll(){
         System.out.println("READ ALL");
-        return null;
+        List<TodoEntitiy> list = this.service.searchAll();
+        List<TodoResponse> response = list.stream().map(TodoResponse::new)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(response);
     }
 
     @PatchMapping("{id}")
-    public ResponseEntity<TodoResponse> update(){
+    public ResponseEntity<TodoResponse> update(@PathVariable Long id, @RequestBody TodoRequest request){
         System.out.println("UPDATE");
-        return null;
+        TodoEntitiy result = this.service.updateById(id,request);
+        return ResponseEntity.ok(new TodoResponse(result));
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<?> deleteOne(){
+    public ResponseEntity<?> deleteOne(@PathVariable Long id){
         System.out.println("DELETE");
-        return null;
+        this.service.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping
     public ResponseEntity<?> deleteAll(){
         System.out.println("DELETE ALL");
-        return null;
+        this.service.deleteAll();
+        return ResponseEntity.ok().build();
     }
     //postman으로 확인할거임
 
